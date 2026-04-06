134cfadc7a8d530a06403587a38ae4164f63a4ca
ajayanandSterco <ajay.anand@stercodigitex.com>
2026-04-06 14:31:58 +0530

    changes

--- src/components/ScriptLoader.js
+++ src/components/ScriptLoader.js
@@ -5,10 +5,7 @@ import Script from "next/script";
 export default function ScriptLoader() {
   return (
     <>
-      <Script
-        src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"
-        strategy="afterInteractive"
-      />
+      <Script src="/js/swiper-bundle.min.js" strategy="afterInteractive" />
       <Script src="/js/custom.js" strategy="afterInteractive" />
     </>
   );

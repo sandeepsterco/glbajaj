bc7c1edba540bfddae4ca66323092ecab677561f
ajayanandSterco <ajay.anand@stercodigitex.com>
2026-04-06 13:54:09 +0530

    update next config file

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,30 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["localhost", "project-demo.in", "glbajaj-topaz.vercel.app"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "sd7",
+        port: "8080",
+        pathname: "/jss/assets/img/banners/**",
+      },
+      {
+        protocol: "https",
+        hostname: "glbajaj-topaz.vercel.app",
+        pathname: "/**",
+      },
+    ],
+    domains: ["sd7", "localhost", "glbajaj-topaz.vercel.app"],
+    unoptimized: true,
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://project-demo.in/gl-bajaj/api/:path*",
+      },
+    ];
   },
 };
 

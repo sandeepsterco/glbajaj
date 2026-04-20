c1908807ca52efca8b88435f30e26e36c00a719e
sandeepsterco <sandeep.rawat@stercodigitex.com>
2026-04-20 10:55:52 +0530

    fix inner not found error

--- src/app/[slug]/layout.tsx
+++ src/app/[slug]/layout.tsx
@@ -1,10 +1,15 @@
 import PageHeader from "@/src/components/layout/header/PageHeader";
 import { apiFetch } from "@/src/lib/api";
+import NotFound from "../not-found";
 
 export default async function InnerPageLayout({children, params}:Readonly<{children:React.ReactNode, params:any}>){
     const {slug} = await params;
 
     const {data, error} = await apiFetch(`cms/${slug}`, { cache:'no-store'});
+
+    if(error){
+        return <NotFound />;
+    }
 
 
     return(

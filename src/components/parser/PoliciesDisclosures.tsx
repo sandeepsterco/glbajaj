c306e8beddb5cf4c54c9fc1bce1cfade2f3c34b0
sandeepsterco <sandeep.rawat@stercodigitex.com>
2026-05-12 15:36:19 +0530

    fix react parser and create module of policies and disclosure

--- src/components/parser/PoliciesDisclosures.tsx
+++ src/components/parser/PoliciesDisclosures.tsx
@@ -3,50 +3,45 @@
 import { BASE_URL } from "@/src/config/config";
 import { apiFetch } from "@/src/lib/api"
 import { useQuery } from "@tanstack/react-query"
-import Image from "next/image";
-import Link from "next/link";
 import { SkeletonGroup } from "../ui/Skeleton";
-import { usePathname } from "next/navigation";
 
-const getConferenceLists = async () => {
-    const { data, error } = await apiFetch(`research-conferences`);
+const getPolicyLists = async () => {
+    const { data, error } = await apiFetch(`policies-pdf`);
 
     if (error) throw new Error(error);
-    return data;
+    return data?.policies_pdf;
 }
 
 export default function PoliciesDisclosures() {
     const { data, isLoading, isError } = useQuery({
-        queryKey: ["conferences-symposium"],
-        queryFn: getConferenceLists
+        queryKey: ["policy-disclosure"],
+        queryFn: getPolicyLists
     })
-
-    const pathname = usePathname();
-    const slug = pathname.split('/').filter(Boolean).pop();
 
     if (isLoading) {
         return (
-            <SkeletonGroup wrapperClassName="mt-[7.7rem] grid-cols-3 gap-[4rem]" count={6} className="bg-gray-300 h-[40rem] w-[100%]" />
+            <SkeletonGroup wrapperClassName="mt-[7.7rem] !block" count={5} className="bg-gray-300 h-[10rem] w-[100%] !mb-[1rem] block" />
         );
     }
 
-    const conferenceData = data?.research_conferences;
+    const policiesData = data?.data;
 
     return (
-        <div>
-            <div className="fac_policy_list" data-aos="fade-up" data-aos-delay="600">
-                <div className="fac_policy_left">
-                    <h5>test</h5>
-                    <p>test date</p>
+        <>
+            {policiesData?.length > 0 && policiesData?.map((item:any, idx:number)=>(
+                <div key={idx} className="fac_policy_list" data-aos="fade-up" data-aos-delay={600+((idx+1)*100)}>
+                    <div className="fac_policy_left">
+                        <h5>{item?.title}</h5>
+                    </div>
+                    <div className="fac_policy_right">
+                        <figure>
+                            <img src="https://project-demo.in/gl-bajaj/assets/img/page-file/1778482442_OwH01DuXvRFZa392zsX9.svg" className="img-fluid" alt="pdf" />
+                        </figure>
+                        <p>Download</p>
+                    </div>
+                    <a target="_blank" href={item.pdf} className="strech_link"></a>
                 </div>
-                <div className="fac_policy_right">
-                    <figure>
-                        <img src="https://project-demo.in/gl-bajaj/assets/img/page-file/1778482442_OwH01DuXvRFZa392zsX9.svg" className="img-fluid" alt="pdf" />
-                    </figure>
-                    <p>Download</p>
-                </div>
-                <a target="_blank" href="{item.pdf}" className="strech_link"></a>
-            </div>
-        </div>
+            ))}
+        </>
     )
 }

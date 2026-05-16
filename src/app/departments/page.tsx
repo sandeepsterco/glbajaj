e566f8f985dc1c4df77f17173b1c0e098e4af874
sandeepsterco <sandeep.rawat@stercodigitex.com>
2026-05-16 17:17:25 +0530

    fix departments grid page

--- src/app/departments/page.tsx
+++ src/app/departments/page.tsx
@@ -3,6 +3,7 @@ import PageHeader from "@/src/components/layout/header/PageHeader";
 import { BASE_URL } from "@/src/config/config";
 import { apiFetch } from "@/src/lib/api";
 import { getSlug } from "@/src/lib/getSlug";
+import Image from "next/image";
 import Link from "next/link";
 
 export default async function DepartmentsPage() {
@@ -25,7 +26,7 @@ export default async function DepartmentsPage() {
                         {departmentData?.data && departmentData.data.map((item:any, idx:number)=>(
                             <div key={idx} className="dept_gbox relative">
                                 <figure>
-                                    <img src={item.image} alt={item.name} />
+                                    <Image src={item?.image ?? '/images/default/department-project.webp'} alt={item.name} height={600} width={850} />
                                 </figure>
                                 <h3 className="font24">{item.name}</h3>
                                 <Link className="strech_link" href={`${BASE_URL}department/${item.slug}`} />

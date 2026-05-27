d7982d2fce48c970b28207c802de360f3bcea888
sandeepsterco <sandeep.rawat@stercodigitex.com>
2026-05-27 12:12:05 +0530

    create admission overview page

--- src/components/faculty/FacultyDetail.tsx
+++ src/components/faculty/FacultyDetail.tsx
@@ -4,56 +4,59 @@ import ReactParser from "../common/reactParser/ReactParser";
 export default function FacultyDetail({ data }: { data: any }) {
     return (
         <section className="faculty-sec">
-            <div className="container55">
+            <div className="container25">
                 <div className="faculty-img1">
                     <figure>
                         <img src="/images/faculty1.png" alt="data" />
                     </figure>
                 </div>
                 <div className="faculty-text">
-                    {data?.name && (
-                        <h2 className="title48">{data.name}</h2>
+                    {data?.data?.name && (
+                        <h2 className="title48">{data.data.name}</h2>
                     )}
-                    {data?.type && (
-                        <p>{data.type}</p>
+                    {data?.data?.type && (
+                        <p>{data.data.type}</p>
                     )}
                 </div>
 
 
                 <div className="faculty-social">
-                    {data?.experience && (
+                    {data?.data?.experience && (
                         <div className="social-box">
                             <figure>
                                 <img src="/images/icons/calendar.svg" alt="calendar" />
                             </figure>
                             <blockquote>Experience</blockquote>
-                            <p>{data.experience}</p>
+                            <p>{data.data.experience}</p>
                         </div>
                     )}
                     
-                    {data?.email && (
+                    {data?.data?.email && (
                         <div className="social-box">
                             <figure>
                                 <img src="/images/icons/email.svg" alt="calendar" />
                             </figure>
                             <blockquote>Email</blockquote>
-                            <p><a href={`mailto:${data.email}`}>{data.email}</a></p>
+                            <p><a href={`mailto:${data.data.email}`}>{data.data.email}</a></p>
                         </div>
                     )}  
                     
-                    {data?.linkedin_url && (
+                    {data?.data?.linkedin_url && (
                         <div className="social-box">
                             <figure>
                                 <img src="/images/icons/linkedin.svg" alt="calendar" />
                             </figure>
                             <blockquote>LinkedIn</blockquote>
-                            <p><Link href={data.linkedin_url} target="_blank">{data.linkedin_url}</Link></p>
+                            <p><Link href={data.data.linkedin_url} target="_blank">{data.data.linkedin_url}</Link></p>
                         </div>
                     )}
 
                 </div>
 
-                <ReactParser html={data.faculty_detail} />
+                {data?.cms?.faculty_detail && (
+                    <ReactParser html={data.cms.faculty_detail} />
+                )}
+
 
             </div>
         </section>

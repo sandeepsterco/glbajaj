0d2752becd86e7856fe5cdbd191383f5020c335d
ashwani kumar <ashwani.kumar@stercodigitex.com>
2026-05-29 09:32:28 +0530

    worked on feedbacks

--- src/components/common/comingSoon/ComingSoon.tsx
+++ src/components/common/comingSoon/ComingSoon.tsx
@@ -4,7 +4,7 @@ export default function ComingSoon() {
   return (
     <main className="coming_soon_page flex-grow flex items-center justify-center px-6 py-24 sm:py-32 lg:px-8 bg-gray-50 h-[calc(100vh-38.3rem)]">
       <div className="text-center">
-        <h1 className="mt-4 text-[5rem] md:text-[10rem] font-bold tracking-tight text-gray-900 uppercase">
+        <h1 className="mt-4 text-[5rem] md:text-[10rem] font-bold tracking-tight text-gray-900  title48">
           Coming Soon
         </h1>
         <p className="mt-6 text-[2rem] md:text-[3rem] leading-[4rem] text-gray-600 max-w-[80rem] mx-auto">
@@ -13,7 +13,7 @@ export default function ComingSoon() {
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <Link
             href="/"
-            className="rounded-md bg-blue-600 px-[2rem] md:px-[3rem] py-[1rem] md:py-[1.5rem] text-[2rem] md:text-[2.5rem] font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 transition duration-200"
+            className="px-[2rem] md:px-[3rem] py-[1rem] md:py-[1.5rem] text-[2rem] md:text-[2.5rem] font-semibold shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 transition duration-200 go_back__btn"
           >
             Go back home
           </Link>

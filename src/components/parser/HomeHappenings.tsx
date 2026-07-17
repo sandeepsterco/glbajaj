9a0e13b4f6f7bd0e96807baa491f3187675bb398
sandeepsterco <sandeep.rawat@stercodigitex.com>
2026-07-17 12:39:12 +0530

    fix happenings css

--- src/components/parser/HomeHappenings.tsx
+++ src/components/parser/HomeHappenings.tsx
@@ -42,7 +42,7 @@ export default function HomeHappenings() {
   return (
     <div className="grid_data">
       {updatedData && updatedData.length > 0 && updatedData.map((item:any, singleIdx:number)=>(
-        <div key={singleIdx} className={`single_grid ${!item?.image && item?.image=="" ? 'no_image' : ''}`} data-aos="fade-up" data-aos-delay="200">
+        <div key={singleIdx} className={`single_grid ${!item?.image && item?.image=="" ? 'no_image' : ''}`} data-aos="fade-up" data-aos-delay="200" style={{backgroundColor:item?.bg_color ?? ''}}>
           {item?.image && (
             <figure>
               <Image src={item.image} alt="happening image" className="img-fluid w-100 " width={636} height={443} loading="lazy" data-aos="fade-up" data-aos-delay="200"/>

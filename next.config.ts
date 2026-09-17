6e4bb286d0d3591602ac48f365ada2e36f00d235
sandeepsterco <sandeep.rawat@stercodigitex.com>
2026-09-17 16:16:11 +0530

    update next file

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ["isomorphic-dompurify", "jsdom"],
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**" },

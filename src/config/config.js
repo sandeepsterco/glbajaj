5e70851c8d715b260f1289ef3f3ac1bbb0cfc411
sandeepsterco <sandeep.rawat@stercodigitex.com>
2026-09-26 15:33:02 +0530

    change config

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const DEFAULT_API_URL = "https://project-demo.in/glbitm/api/";
+export const API_URL = (process.env.NEXT_PUBLIC_API_URL?.trim() || DEFAULT_API_URL).replace(/\/+$/, "") + "/";
 export const SEO_URL = process.env.NEXT_PUBLIC_SEO_URL;
 export const APPLY_NOW = process.env.NEXT_PUBLIC_APPLY_NOW;
 export const REVALIDATE = process.env.NEXT_PUBLIC_REVALIDATE;
